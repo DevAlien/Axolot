df6aac3ce2b7a1c5e385bbaca5b6c672f7efedf3
Gonçalo Margalho <gsky89@gmail.com>
2015-03-01 22:17:25 +0100

    Add app into Services

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ orm.initialize(config.orm, function (err, models) {
         global.Service = {};
         _(services).each(function (service, key) {
             app.services[key] = service;
+            service.app = app.config;
             global.Service[key] = service;
         });
 
